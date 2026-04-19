a4d3d9dda88e438225aba65f757dd5e18e92e7c2
ALAOUI MRANI Sana <san.alaouimrani@edu.umi.ac.ma>
2026-04-19 21:23:30 +0100

    replacing assert of numero by adding pattern

--- contact.py
+++ contact.py
@@ -4,8 +4,9 @@ class Contact:
     def __init__(self, nom: str, email: str, numéro: str):
         assert isinstance(nom, str) and nom.strip(), "Le nom doit être une chaîne non vide."
         pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        pattern_numéro = r"^(06|07)\d{8}$"
         assert re.fullmatch(pattern, email) is not None, "Adresse email invalide."
-        assert numéro.isdigit() and len(numéro) == 10 and (numéro.startswith("06") or numéro.startswith("07")), \
+        assert re.fullmatch(pattern_numéro, numéro) is not None, \
             "Le numéro doit comporter 10 chiffres et commencer par 06 ou 07."
 
         self.nom = nom.strip()
